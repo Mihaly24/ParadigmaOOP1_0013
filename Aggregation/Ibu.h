5c68c082273ba0f3a47313695c2d2e2c7bcaec3c
Mihaly24 <fairuzk20@gmail.com>
2024-05-21 20:20:28 +0700

    Mengubah typo dan kesalahan pada header ibu

--- Aggregation/Ibu.h
+++ Aggregation/Ibu.h
@@ -5,25 +5,25 @@
 class ibu {
 public:
 	string nama;
-	vector<Anak*> daftar_anak;
+	vector<anak*> daftar_anak;
 
 	ibu(string pNama) :nama(pNama) {
 		cout << "Ibu \"" << nama << "\" ada \n";
 	}
 	~ibu() {
-		cout << "Ibu \"" << nama << "\" tidka ada\n";
+		cout << "Ibu \"" << nama << "\" tidak ada\n";
 	}
-	void tambahAnak(Anak*);
+	void tambahAnak(anak*);
 	void cetakAnak();
 };
-void ibu::tambahAnak(Anak* pAnak) {
+void ibu::tambahAnak(anak* pAnak) {
 	daftar_anak.push_back(pAnak);
 }
 void ibu::cetakAnak() {
 	cout << "Daftar Anak dari ibu \"" << this->nama << "\":\n";
-	\* for (auto& a : daftar_anak) {
+	for (auto& a : daftar_anak) {
 		cout << a->nama << "\n";
-	}*\
+	}
 	for (int i = 0; i < daftar_anak.size(); i++) {
 			cout << daftar_anak[i]->nama << endl;
 	}
